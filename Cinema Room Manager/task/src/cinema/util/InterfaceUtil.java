099284c5db2000f3e889dabff5d074e660ad9aab
Dalahan151 <dalahan452@gmail.com>
2024-08-31 00:09:50 +0300

    Created simple interface using the class InterfaceUtil.java

---
+++ Cinema Room Manager/task/src/cinema/util/InterfaceUtil.java
@@ -0,0 +1,10 @@
+package cinema.util;
+
+public class InterfaceUtil {
+
+    public static void printInterface() {
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("0. Exit");
+    }
+}
